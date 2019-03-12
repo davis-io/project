d037e7accc3e2c0d451eba2712d07bd3a2abb340
Rodrigo González Laiz <31796689+gonlairo@users.noreply.github.com>
2019-03-11 20:42:29 -0700

    Update map.R

--- map.R
+++ map.R
@@ -6,18 +6,23 @@ library(urbnthemes) # don't install it
 library(gganimate)
 library(plotly)
 
+####################################################################################
+####################################################################################
+
+#example with data from urbnmapr
+
 #countydata has to be my data
 countydata %>% 
   left_join(counties, by = "county_fips") %>% 
   filter(state_name =="California") %>% 
-  ggplot(mapping = aes(long, lat, group = group, fill = medhhincome)) + ## medhhincome has to be our total_obligation
+  ggplot(mapping = aes(long, lat, group = group, fill = medhhincome)) + # medhhincome has to be our total_obligation
   
   # the color in the polygon is the borders between states
   #size: thickness of the border
-  geom_polygon(color = "#FFA500", size = .45) +                        # or other variables we want to understand
+  geom_polygon(color = "#FFA500", size = .45) +                        
   scale_fill_gradientn(labels = scales::percent,
                        guide = guide_colorbar(title.position = "top"),
-                       colours = terrain.colors(13))  +# colours controls the gradient of colors in the map
+                       colours = terrain.colors(13))  +  # colours controls the gradient of colors in the map
   
   coord_map(projection = "albers", lat0 = 39, lat1 = 45) + #projects a portion of the earth, which is approximately spherical, onto a flat 2D plane 
   
@@ -25,9 +30,6 @@ countydata %>%
         legend.key.width = unit(.5, "in"))   +
   labs(fill = "Median income") + #change the name of the variable medhhincome to Homeownership rate
   #set_urbn_defaults() this is specific to urbnthemes
-
-
-
 
 
 ####################################################################################
@@ -65,6 +67,7 @@ df_map_2015 = df_map[df_map$fiscal_year == 2015 , ]
 # map
 # it is not perfect and there may be errors. 
 # we need to customize it
+# we need to animate it with plotly
 
 ggplot(mapping = aes(df_map_2015$long, df_map_2015$lat, group = df_map_2015$group, fill = df_map_2015$total_obligation)) + 
   geom_polygon(color = "#FFA500", size = .45) +                        
